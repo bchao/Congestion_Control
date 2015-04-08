fa899a19513fbffc3dee17b91a67145d5b1b43cf
Peter Yom <ypeter999@gmail.com>
2015-04-08 02:25:19 -0400

    3b stuff

--- 3b/reliable/reliable.c
+++ 3b/reliable/reliable.c
@@ -23,6 +23,26 @@ struct reliable_state {
 
   /* Add your own data fields below this */
 
+  wrapper **sentPackets, **recvPackets;
+  int sentListSize, recvListSize;
+
+  int windowSize;
+  // Save retransmission timeout from config_common
+  int timeout;
+
+  // Sending side
+  int LAST_PACKET_ACKED;
+  int LAST_PACKET_SENT;
+
+  // Receiving side
+  int NEXT_PACKET_EXPECTED;
+
+  int eofSent, eofRecv;
+  /* Client */
+  int client_state;
+
+  /* Server */
+  int server_state;
 };
 rel_t *rel_list;
 
@@ -51,11 +71,34 @@ rel_create (conn_t *c, const struct sockaddr_storage *ss,
     }
   }
 
-  r->c = c;
+  r->c = c;  
   rel_list = r;
 
   /* Do any other initialization you need here */
+  r->windowSize = cc->window;
+  r->timeout = cc->timeout;
 
+  r->sentListSize = 0;
+  r->recvListSize = 0;
+
+  r->sentPackets = malloc(sizeof(wrapper *) * r->windowSize);
+  r->recvPackets = malloc(sizeof(wrapper *) * r->windowSize);
+
+  int i;
+  for (i = 0; i < r->windowSize; i++) {
+    r->sentPackets[i] = malloc(sizeof(wrapper));
+    r->sentPackets[i]->packet = malloc(sizeof(packet_t));
+    r->recvPackets[i] = malloc(sizeof(wrapper));
+    r->recvPackets[i]->packet = malloc(sizeof(packet_t));
+  }
+
+  r->LAST_PACKET_ACKED = 0;
+  r->LAST_PACKET_SENT = 0;
+
+  r->NEXT_PACKET_EXPECTED = 1;
+
+  r->eofSent = 0;
+  r->eofRecv = 0;
 
   return r;
 }
@@ -66,6 +109,16 @@ rel_destroy (rel_t *r)
   conn_destroy (r->c);
 
   /* Free any other allocated memory here */
+  int i;
+  for (i = 0; i < r->windowSize; i++) {
+    free(r->sentPackets[i]->packet);
+    free(r->sentPackets[i]);
+    free(r->recvPackets[i]->packet);
+    free(r->recvPackets[i]);
+  }
+  free(r->sentPackets);
+  free(r->recvPackets);
+  free(r);  
 }
 
 
@@ -80,9 +133,92 @@ rel_demux (const struct config_common *cc,
 void
 rel_recvpkt (rel_t *r, packet_t *pkt, size_t n)
 {
+  uint16_t len = ntohs(pkt->len);
+  uint32_t ackno = ntohl(pkt->ackno);
+
+  int verified = verifyChecksum(r, pkt, n);
+  if (!verified || (len != n)) { // drop packets with bad length
+    return;
+  }
+
+  if (len == ACK_PACKET_SIZE) { // ack packet
+
+    if (ackno <= r->LAST_PACKET_ACKED) { //duplicate ack
+      return;
+    }
+    // ack packet
+    if (ackno == r->LAST_PACKET_SENT + 1) { // Should be changed to LAST_PACKET_ACKED + 1 later?
+      // this is the expected in order ack number
+      r->LAST_PACKET_ACKED++;
+
+      // "delete" previous value
+      shiftPacketList(r, pkt);
+
+      rel_read(r);
+    }
+    else {
+      // packets preceding this were dropped
+    }
+  }
+  else if (len == HEADER_SIZE) { // eof condition, destroy??
+    // signal to destroy? send eof?
+
+    uint32_t seqno = ntohl(pkt->seqno);
+    if (seqno == r->NEXT_PACKET_EXPECTED) {
+      struct ack_packet *ack = createAckPacket(r);
+
+      conn_sendpkt(r->c, (packet_t *)ack, ACK_PACKET_SIZE);
+      conn_output(r->c, pkt->data, len - HEADER_SIZE);
+      r->NEXT_PACKET_EXPECTED++;
+      free(ack);
+      // rel_destroy(r);
+    } 
+  }
+  else { // data packet
+    uint32_t seqno = ntohl(pkt->seqno);
+
+    if (seqno < r->NEXT_PACKET_EXPECTED - 1) { // duplicate packet
+      return;
+    }
+    if (seqno - r->NEXT_PACKET_EXPECTED > r->windowSize) { // packet outside window
+      return;
+    }
+    // holds data that arrives out of order and data that is in correct order, but app hasn't read yet
+    // memcpy(r->recvPackets[0]->packet, pkt, sizeof(packet_t));
+    // rel_output(r);
+
+    // size_t availableLength = conn_bufspace(r->c);
+    if (seqno == r->NEXT_PACKET_EXPECTED) {
+      struct ack_packet *ack = createAckPacket(r);
+
+      conn_sendpkt(r->c, (packet_t *)ack, ACK_PACKET_SIZE);
+      conn_output(r->c, pkt->data, len - HEADER_SIZE);
+      // rel_output(r);
+      r->NEXT_PACKET_EXPECTED++;
+      free(ack);
+    }
+    else {
+      // store in buffer
+
+      // int slot = seqno - r->NEXT_PACKET_EXPECTED + 1;
+      // memcpy(r->recvPackets[slot]->packet, pkt, sizeof(packet_t));
+      // r->recvPackets[slot]->sentTime = getCurrentTime();
+    }
+  }  
 }
 
-
+/*
+If the reliable program is running in the receiver mode 
+(see c.sender_receiver in rlib.c, you can get its value in 
+rel_create), this receiver should send an EOF to the sender 
+when rel_read is first called. After this first call, the 
+function rel_read can simply return for later calls. Note 
+that this EOF will wait in the receiver's sending window. 
+When timeout happens, the receiver have to retransmit this 
+EOF as well until an ACK is received. If the reliable is 
+running in the sender mode, the rel_read's behavior is the 
+same as that is described above in 3a.
+*/
 void
 rel_read (rel_t *s)
 {
@@ -95,7 +231,48 @@ rel_read (rel_t *s)
   }
   else //run in the sender mode
   {
-    //same logic as lab 1
+    int numPacketsInWindow = s->LAST_PACKET_SENT - s->LAST_PACKET_ACKED;
+    if (numPacketsInWindow >= s->windowSize || s->eofSent) {
+      // don't send, window's full, waiting for acks
+      return;
+    }
+
+    if (numPacketsInWindow == 0 && s->eofSent == 1 && s->eofRecv == 1) {
+      rel_destroy(s);
+      return;
+    }
+
+    // can send packet
+    char payloadBuffer[MAX_PAYLOAD_SIZE];
+
+
+    int bytesReceived = conn_input(s->c, payloadBuffer, MAX_PAYLOAD_SIZE);
+    if (bytesReceived == 0) {
+      return; // no data is available at the moment, just return
+    }
+    else if (bytesReceived == -1) { // eof or error
+      s->eofSent = 1;
+      bytesReceived = 0;
+
+      // Why do we need to create and send a packet here?
+
+      packet_t *packet = createDataPacket(s, payloadBuffer, bytesReceived);
+      conn_sendpkt(s->c, packet, HEADER_SIZE + bytesReceived);
+      free(packet);
+      return;
+    }
+
+    // TODO: Need to handle overflow bytes here as well
+
+    packet_t *packet = createDataPacket(s, payloadBuffer, bytesReceived);
+
+    // Save packet until it's acked/in case it needs to be retransmitted
+    memcpy(s->sentPackets[numPacketsInWindow]->packet, packet, HEADER_SIZE + bytesReceived);
+    s->sentPackets[numPacketsInWindow]->sentTime = getCurrentTime();
+
+    s->LAST_PACKET_SENT++;
+    conn_sendpkt(s->c, packet, HEADER_SIZE + bytesReceived);
+    free(packet);
   }
 }
 
